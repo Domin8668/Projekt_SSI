2a90de2341105c798bc1b8ef5d71db29fd207c2c
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2022-05-03 21:26:05 +0200

    Cleaned up some code

--- fuzzySystem.py
+++ fuzzySystem.py
@@ -1,77 +1,76 @@
-import numpy as np
-import skfuzzy as fuzz
-from skfuzzy import control as ctrl
-
 class Fuzzy:
     def __init__(self):
-        self.antecedent=[]
-        self.consequent=[]
-        self.rules=[]
-    def triangularFunction(self,x,a,b,c):
-        if x<=a:
-            return 0
-        if x>a and x<=b:
-            return (x-a)/(b-a)
-        if x>b and x<=c:
-            return (c-x)/(c-b)
-        if x>c:
-            return 0
-    def addAntecedent(self,parameter, linguisticValue,a,b,c):
-        self.antecedent.append([parameter,linguisticValue,a,b,c])
-    def addRule(self,rule):
-        self.rules.append(rule)
-    def addConsequent(self,linguisticValue,parameters):
-        self.consequent.append([linguisticValue,parameters])
-        
-    def compute(self,sample):
-        #rozmycie
-        fuzzyValues=[]
-        for i in self.antecedent:
-            fuzzyValues.append([i[0],i[1],self.triangularFunction(sample[i[0]],i[2],i[3],i[4])])
+        self.antecedent = []
+        self.consequent = []
+        self.rules = []
 
-        #wnioskowanie
-        rulesResult=[]
+    @staticmethod
+    def triangular_function(x, a, b, c):
+        if x <= a:
+            return 0
+        if a < x <= b:
+            return (x - a) / (b - a)
+        if b < x <= c:
+            return (c - x) / (c - b)
+        if x > c:
+            return 0
+
+    def add_antecedent(self, parameter, linguisticValue, a, b, c):
+        self.antecedent.append([parameter, linguisticValue, a, b, c])
+
+    def add_rule(self, rule):
+        self.rules.append(rule)
+
+    def add_consequent(self, linguisticValue, parameters):
+        self.consequent.append([linguisticValue, parameters])
+
+    def compute(self, sample):
+        # rozmycie
+        fuzzy_values = []
+        for i in self.antecedent:
+            fuzzy_values.append([i[0], i[1], self.triangular_function(sample[i[0]], i[2], i[3], i[4])])
+
+        # wnioskowanie
+        rules_result = []
         for i in self.rules:
-            tmp=1
-            for j in fuzzyValues:
-                if i[j[0]]==j[1]:
-                    tmp*=j[2] 
-            rulesResult.append(tmp)
-        print('rulesResult:',rulesResult)
-        #wyostrzenie
-        res=max (rulesResult)
-        decision=''
-        decision=self.rules[rulesResult.index(res)]['jakosc zycia']
-        print("W miescie",sample['miasto'], "jakosc zycia jest",decision)
+            tmp = 1
+            for j in fuzzy_values:
+                if i[j[0]] == j[1]:
+                    tmp *= j[2]
+            rules_result.append(tmp)
+        print('rulesResult:', rules_result)
+        # wyostrzenie
+        res = max(rules_result)
+        decision = self.rules[rules_result.index(res)]['jakosc zycia']
+        print("W miescie", sample['miasto'], "jakosc zycia jest", decision)
         print()
 
-        
-system=Fuzzy()
-system.addAntecedent('naslonecznienie','zle',0,0,0.4)
-system.addAntecedent('naslonecznienie','srednie',0.3,0.4,0.8)
-system.addAntecedent('naslonecznienie','dobre',0.7,1,1)
 
-system.addAntecedent('skazenie','wysokie',0,0,0.4)
-system.addAntecedent('skazenie','srednie',0.3,0.4,0.8)
-system.addAntecedent('skazenie','niskie',0.7,1,1)
+system = Fuzzy()
+system.add_antecedent('naslonecznienie', 'zle', 0, 0, 0.4)
+system.add_antecedent('naslonecznienie', 'srednie', 0.3, 0.4, 0.8)
+system.add_antecedent('naslonecznienie', 'dobre', 0.7, 1, 1)
 
-system.addConsequent('jakosc zycia',[['zla',0,0,0.4],['srednia',0.3,0.6,0.8],['dobra',0.7,1,1]])
+system.add_antecedent('skazenie', 'wysokie', 0, 0, 0.4)
+system.add_antecedent('skazenie', 'srednie', 0.3, 0.4, 0.8)
+system.add_antecedent('skazenie', 'niskie', 0.7, 1, 1)
 
-#regul powinno byc 9
-system.addRule({'naslonecznienie':'zle','skazenie':'wysokie','jakosc zycia':'zla'})
-system.addRule({'naslonecznienie':'srednie','skazenie':'wysokie','jakosc zycia':'zla'})
-system.addRule({'naslonecznienie':'zle','skazenie':'srednie','jakosc zycia':'zla'})
-system.addRule({'naslonecznienie':'zle','skazenie':'niskie','jakosc zycia':'srednia'})
-system.addRule({'naslonecznienie':'dobre','skazenie':'wysokie','jakosc zycia':'srednia'})
-system.addRule({'naslonecznienie':'srednie','skazenie':'srednie','jakosc zycia':'srednia'})
-system.addRule({'naslonecznienie':'srednie','skazenie':'niskie','jakosc zycia':'dobra'})
-system.addRule({'naslonecznienie':'dobre','skazenie':'srednie','jakosc zycia':'dobra'})
-system.addRule({'naslonecznienie':'dobre','skazenie':'niskie','jakosc zycia':'dobra'})
+system.add_consequent('jakosc zycia', [['zla', 0, 0, 0.4], ['srednia', 0.3, 0.6, 0.8], ['dobra', 0.7, 1, 1]])
 
-miasta=[]
-miasta.append({'naslonecznienie':0.6,'skazenie':0.3,'miasto':'Warszawa'})
-miasta.append({'naslonecznienie':1.0,'skazenie':0.1,'miasto':'Krakow'})
+# regul powinno byc 9
+system.add_rule({'naslonecznienie': 'zle', 'skazenie': 'wysokie', 'jakosc zycia': 'zla'})
+system.add_rule({'naslonecznienie': 'srednie', 'skazenie': 'wysokie', 'jakosc zycia': 'zla'})
+system.add_rule({'naslonecznienie': 'zle', 'skazenie': 'srednie', 'jakosc zycia': 'zla'})
+system.add_rule({'naslonecznienie': 'zle', 'skazenie': 'niskie', 'jakosc zycia': 'srednia'})
+system.add_rule({'naslonecznienie': 'dobre', 'skazenie': 'wysokie', 'jakosc zycia': 'srednia'})
+system.add_rule({'naslonecznienie': 'srednie', 'skazenie': 'srednie', 'jakosc zycia': 'srednia'})
+system.add_rule({'naslonecznienie': 'srednie', 'skazenie': 'niskie', 'jakosc zycia': 'dobra'})
+system.add_rule({'naslonecznienie': 'dobre', 'skazenie': 'srednie', 'jakosc zycia': 'dobra'})
+system.add_rule({'naslonecznienie': 'dobre', 'skazenie': 'niskie', 'jakosc zycia': 'dobra'})
 
-for sample in miasta:
-    print(sample)
-    system.compute(sample)
+miasta = [{'naslonecznienie': 0.6, 'skazenie': 0.3, 'miasto': 'Warszawa'},
+          {'naslonecznienie': 1.0, 'skazenie': 0.1, 'miasto': 'Krakow'}]
+
+for miasto in miasta:
+    print(miasta)
+    system.compute(miasta)
